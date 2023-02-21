18931f8c5e20fd2175b2ec89a36f936070a43bc9
OteroG-27 <coterocarranza@gmail.com>
2023-02-21 08:22:52 -0500

    funcionalidad de fecha ready

---
+++ src/date.js
@@ -0,0 +1,43 @@
+function startTime() {
+  let today = new Date();
+  let months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  let days = [
+    "Lunes",
+    "Martes",
+    "MIercoles",
+    "Jueves",
+    "Viernes",
+    "Viernes",
+    "Sabado",
+  ];
+  let curWeekDay = days[today.getDay()];
+  let curDay = today.getDate();
+  let curMonth = months[today.getMonth()];
+  let curYear = today.getFullYear();
+  let date = curWeekDay + ", " + curDay + " " + curMonth + " " + curYear;
+  document.getElementById("date").innerHTML = date;
+
+  let time = setTimeout(function () {
+    startTime();
+  }, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
