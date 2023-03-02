1eb110d15b6a3f83553505d25142464321970d72
OteroG-27 <coterocarranza@gmail.com>
2023-03-01 20:04:47 -0500

    ya la funcion maskify esta lista

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import validator from './validator.js';
 import {startTime, checkTime} from './date.js';
 window.addEventListener('DOMContentLoaded', startTime, checkTime);
 // validator.isValid('4539148803436467');
-validator.maskify('4539148803436467');
+
 
 let validate = document.getElementById('validate');
 validate.addEventListener('click', (event) => {
   event.preventDefault();
   let numberTargetCredit = document.getElementById('numberTarget').value;
-  validator.isValid(numberTargetCredit);
-  document.getElementById('numberTargetSee').innerHTML = numberTargetCredit;
+  let isValid = validator.isValid(numberTargetCredit);
+  let numeroConNumeral = validator.maskify(numberTargetCredit).join('');
+  if(isValid){
+    document.getElementById('numberTargetSee').innerHTML = numeroConNumeral;
+    document.getElementById('numberTarget').classList.remove('numberInvalid');
+    document.getElementById('numberTarget').classList.add('numberValid');
+    alert('Numero valido')
+  }else{
+    document.getElementById('numberTargetSee').innerHTML = numeroConNumeral;
+    document.getElementById('numberTarget').classList.add('numberInvalid');
+    document.getElementById('numberTarget').classList.remove('numberValid');
+    alert('Introduzca un numero valido')
+
+  }
 });
+
--- src/validator.js
+++ src/validator.js
@@ -3,32 +3,6 @@ const validator = {
     const newArray = CreditCardNumber.split('').reverse();
     let par = [],
         impar= [];
-    // const card = newArray.map((number, index) => {
-    //   if(index % 2 === 0){
-    //     let numXDos = number * 2;
-    //     if(numXDos > 9) {
-    //       let numResta = numXDos - 9;
-    //       par.push(parseInt(numResta))
-    //     }else{
-    //       par.push(parseInt(numXDos))
-    //     }
-    //   }else{
-    //     impar.push(parseInt(number))
-    //   }
-    // });
-    // let fucionArrays = [...par, ...impar];
-    // const sumaNumberCard = fucionArrays.reduce(
-    //   (acumulador, valoractual) => acumulador + valoractual, 0);
-    // if (sumaNumberCard  % 10 === 0 ) {
-    //   alert("Numero de targeta valido")
-    //   // document.getElementById('numberTarget').classList.remove('numberInvalid');
-    //   // document.getElementById('numberTarget').classList.add('numberValid');
-    // } else {
-    //   alert("Numero de targeta invalido")
-    //   // document.getElementById('numberTarget').classList.add('numberInvalid');
-    //   // document.getElementById('numberTarget').classList.remove('numberValid');
-    // }
-    console.log(newArray);
     const isValidNumber = newArray.filter((number, i) =>  i % 2 == 0 ).map(number => number * 2);
     // console.log(isValidNumber);
     isValidNumber.forEach(newNumber => {
@@ -39,28 +13,17 @@ const validator = {
         impar.push(parseInt(newNumber))
       }
     });
-    // console.log(par);
-    // console.log(impar);
     let fucionArrays = [...par, ...impar];
-    // console.log(fucionArrays);
     const sumaNumberCard = fucionArrays.reduce(
       (acumulador, valoractual) => acumulador + valoractual, 0);
     if (sumaNumberCard  % 10 === 0 ) {
       alert("Numero de targeta valido")
-      // document.getElementById('numberTarget').classList.remove('numberInvalid');
-      // document.getElementById('numberTarget').classList.add('numberValid');
     } else {
       alert("Numero de targeta invalido")
-      // document.getElementById('numberTarget').classList.add('numberInvalid');
-      // document.getElementById('numberTarget').classList.remove('numberValid');
     }
   },
   maskify(CreditCardNumber){
-    const newArray = CreditCardNumber.split('');
-    console.log(newArray);
-    newArray.fill('#', 0, newArray.length - 4)
-
-    console.log(newArray);
+    return CreditCardNumber.split('').fill('#', 0, CreditCardNumber.length - 4);
   }
 };
 
