1af733ba31e69d5ffc491d6fa835c69e6524aaf5
OteroG-27 <coterocarranza@gmail.com>
2023-03-08 17:37:32 -0500

    Updates

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ let validate = document.getElementById('validate');
 validate.addEventListener('click', (event) => {
   event.preventDefault();
   let numberTargetCredit = document.getElementById('numberTarget').value;
-  validator.isValid(numberTargetCredit);
+  let isValid = validator.isValid(numberTargetCredit);
   let numeroConNumeral = validator.maskify(numberTargetCredit);
-  if(numberTargetCredit) {
+  if(isValid === true && numberTargetCredit === '') {
+    alert("Para validar ingrese mas de 16 caracteres");
+    document.getElementById('numberTarget').classList.add('numberInvalid');
+    document.getElementById('numberTarget').classList.remove('numberValid');
+  }else if(isValid) {
     alert("Numero valido");
+    document.getElementById('numberTargetSee').innerHTML = numeroConNumeral;
     document.getElementById('numberTarget').classList.add('numberValid');
     document.getElementById('numberTarget').classList.remove('numberInvalid');
-    document.getElementById('numberTargetSee').innerHTML = numeroConNumeral;
   }else{
-    alert('Introduzca un numero de tarjeta valido');
+    alert('Numero Invalido');
     document.getElementById('numberTargetSee').innerHTML = numeroConNumeral;
     document.getElementById('numberTarget').classList.add('numberInvalid');
     document.getElementById('numberTarget').classList.remove('numberValid');
   }
+
 });
 
