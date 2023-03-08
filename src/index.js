f2aa258fa8fce03a1c87d6d7f630ea1ee78e00f9
OteroG-27 <coterocarranza@gmail.com>
2023-03-08 14:20:20 -0500

    Se hizo el  README

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import validator from './validator.js';
 import {startTime, checkTime} from './date.js';
 window.addEventListener('DOMContentLoaded', startTime, checkTime);
-// validator.isValid('79927398713');
 let validate = document.getElementById('validate');
 validate.addEventListener('click', (event) => {
   event.preventDefault();
   let numberTargetCredit = document.getElementById('numberTarget').value;
-  let isValid = validator.isValid(numberTargetCredit);
+  validator.isValid(numberTargetCredit);
   let numeroConNumeral = validator.maskify(numberTargetCredit);
-  if(isValid){
+  if(numberTargetCredit) {
     alert("Numero valido");
     document.getElementById('numberTarget').classList.add('numberValid');
     document.getElementById('numberTarget').classList.remove('numberInvalid');
